531d2982e577d827a7ad7f32252e78c61eed479f
Mahendra Mikkilineni <120906154+MikkilineniMahendera@users.noreply.github.com>
2025-05-23 15:14:08 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from Mahendra");
+        System.out.println( "Hello from Mahendra PHANI");
     }
 }
